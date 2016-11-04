6cad0aa418e9b6e44d144b362aa31cbe9ca0c8cd
antonioi520 <amisabella@email.neit.edu>
2016-11-04 18:35:46 -0400

    Add files via upload

---
+++ lab4/app_server/controllers/about.js
@@ -0,0 +1,5 @@
+/* GET home page */
+module.exports.about = function(req, res){
+  res.render('about', { title: 'About' });
+  //res.render('about', { paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mi lectus, ultrices ut neque nec, condimentum pellentesque magna. Nunc placerat mi urna, eu iaculis ex volutpat ac. Aenean sit amet dolor eget augue volutpat tristique ut non velit. Morbi at tellus dolor. Cras sollicitudin elementum erat et volutpat. Nam sed lobortis ex. Nulla gravida pretium hendrerit. Cras dapibus leo eros. Pellentesque fringilla tortor fringilla diam vehicula, quis pellentesque purus ultrices. Aliquam tempor velit ac ante sollicitudin viverra. Curabitur euismod massa ut vehicula blandit.' });
+};
