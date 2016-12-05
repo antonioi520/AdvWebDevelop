80c95e205a9d9ade84605aa5fe28911b67efe79d
antonioi520 <amisabella@email.neit.edu>
2016-12-05 18:49:29 -0500

    Add files via upload

---
+++ lab6/server1/app_api/models/employee.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var employeeSchema = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    department: {type: String, required: true},
+    startDate: {type: Date,  required: true},
+    jobTitle: {type: String, required: true},
+    salary: {type: Number, required: true}
+    
+});
+
+/* This model will also create the collection in the Loc8r database when used */
+var Employee = mongoose.model('Employee', employeeSchema);
